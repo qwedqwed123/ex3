4f13be51a5c556d1a0ec67c4d03fa103584fa388
4b3g0099@office.stust.edu.tw <4b3g0099@office.stust.edu.tw>
2024-11-12 12:03:51 +0800

    非遞迴計算

--- ex3/ex3.cpp
+++ ex3/ex3.cpp
@@ -2,10 +2,20 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    
+    int n;
+    cout << "輸入一個正整數:";
+    cin >> n;
+    //非遞回計算
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        //result *= i;
+        result = result * i;
+    }
+    cout << n << "! = " << result << endl;
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
